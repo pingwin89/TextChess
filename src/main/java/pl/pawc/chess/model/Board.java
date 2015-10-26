cac5b25dfb21f86ea20f168a28f343bc840a526c
pawc <pswid89@gmail.com>
2015-10-26 21:15:03 +0100

    even more figure models

--- src/main/java/pl/pawc/chess/model/Board.java
+++ src/main/java/pl/pawc/chess/model/Board.java
@@ -9,11 +9,26 @@ public class Board {
 	public Board(){
 		board = new Figure[8][8];
 		for(int i=0; i<8; i++){
-			board[0][i] = new Knight(0, i);
 			board[1][i] = new Pawn(1, i);
-			board[6][i] = new Figure(6, i);
-			board[7][i] = new Knight(7, i);
+			board[6][i] = new Pawn(6, i);
 		}
+		board[0][2] = new Knight(0,2);
+		board[0][6] = new Knight(0,6);
+		board[0][0] = new Rook(0,1);
+		board[0][7] = new Rook(0,7);
+		board[0][1] = new Bishop(0,1);
+		board[0][5] = new Bishop(0,5);
+		board[0][3] = new King(0,3);
+		board[0][4] = new Queen(0,4);
+		
+		board[7][2] = new Knight(7,2);
+		board[7][6] = new Knight(7,6);
+		board[7][0] = new Rook(7,1);
+		board[7][7] = new Rook(7,7);
+		board[7][1] = new Bishop(7,1);
+		board[7][5] = new Bishop(7,5);
+		board[7][3] = new King(7,3);
+		board[7][4] = new Queen(7,4);
 	}
 	
 	public void printOut(){
@@ -22,6 +37,11 @@ public class Board {
 				if(board[i][j] != null ){
 					if(board[i][j] instanceof Pawn) Start.logRed("P");
 					if(board[i][j] instanceof Knight) Start.logRed("K");
+					if(board[i][j] instanceof Bishop) Start.logRed("B");
+					if(board[i][j] instanceof Rook) Start.logRed("R");
+					if(board[i][j] instanceof Queen) Start.logRed("Q");
+					if(board[i][j] instanceof King) Start.logRed("+");
+					
 				}
 				else{
 					Start.logNormal("-");
