5b9631df7233cac3695f2dd8fb212f829ac5c32e
pawc <pswid89@gmail.com>
2015-11-04 18:16:12 +0100

    rook move validation test

---
+++ src/test/java/pl/pawc/chess/model/RookTest.java
@@ -0,0 +1,35 @@
+package pl.pawc.chess.model;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class RookTest extends TestCase{
+    public RookTest(String testName){
+        super(testName);
+    }
+
+    public static Test suite(){
+        return new TestSuite(RookTest.class);
+    }
+
+    public void testIsMoveValid(){
+    Board board = new Board();
+	assertFalse(Rook.isMoveValid(7, 0, 5, 0, board));
+	board.move(6, 0, 5, 1);
+	assertTrue(Rook.isMoveValid(7, 0, 5, 0, board));
+	assertFalse(Rook.isMoveValid(0, 7, 3, 7, board));
+	board.move(1, 7, 2, 6);
+	assertTrue(Rook.isMoveValid(0, 7, 2, 7, board));
+	assertFalse(Rook.isMoveValid(2, 7, 2, 5, board));
+	board.move(2, 6, 3, 6);
+	assertTrue(Rook.isMoveValid(2, 7, 2, 5, board));
+	board.move(2, 5, 3, 3);
+	assertFalse(Rook.isMoveValid(3, 3, 4, 4, board));
+	assertFalse(Rook.isMoveValid(3, 3, 2, 2, board));
+	assertFalse(Rook.isMoveValid(3, 3, 4, 2, board));
+	assertFalse(Rook.isMoveValid(3, 3, 2, 4, board));			
+	
+   }
+
+}
