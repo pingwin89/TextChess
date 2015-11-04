0102c8b76721d2fd8a87e0bdcb34ed541fb11039
pawc <pswid89@gmail.com>
2015-11-04 10:14:22 +0100

    added  tests

---
+++ src/test/java/pl/pawc/chess/model/BoardTest.java
@@ -0,0 +1,23 @@
+package pl.pawc.chess.model;
+
+import pl.pawc.chess.model.Board;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class BoardTest extends TestCase{
+    public BoardTest(String testName){
+        super( testName );
+    }
+
+    public static Test suite(){
+        return new TestSuite(BoardTest.class);
+    }
+
+    public void testBoard(){
+	Board board = new Board();
+	assertNotNull(board);
+    }
+
+}
---
+++ src/test/java/pl/pawc/chess/model/FigureTest.java
@@ -0,0 +1,33 @@
+package pl.pawc.chess.model;
+
+import pl.pawc.chess.model.Figure;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class FigureTest extends TestCase{
+    public FigureTest(String testName){
+        super(testName);
+    }
+
+    public static Test suite(){
+        return new TestSuite(FigureTest.class);
+    }
+
+    public void testFigureGetters(){
+        Figure figure = new Figure(1,2,"RED");
+	assertEquals(1, figure.getX());
+	assertEquals(2, figure.getY());
+	assertEquals("RED", figure.getColor());
+    }
+
+    public void testFigureSetters(){
+	Figure figure = new Figure(0,0,"BLUE");
+	figure.setX(1);
+	figure.setY(2);
+	assertEquals(1, figure.getX());
+	assertEquals(2, figure.getY());
+    }
+
+}
